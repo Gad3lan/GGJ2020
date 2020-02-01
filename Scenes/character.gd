ecf1e761c82de8ddd4b54c45f8d35703a38c1d12
unknown <a.marce@free.fr>
2020-02-01 18:09:13 +0100

    modifiying jump

--- Scenes/character.gd
+++ Scenes/character.gd
@@ -1,39 +1,53 @@
 extends KinematicBody2D
 
 export var SPEED = 20
-export var G = 120
+export var JUMP = 80
+export var G = 2
 
-var screen_size
 var velocity = Vector2()
+var on_floor = false
 
-func _ready():
-	screen_size = get_viewport_rect().size
 
 func moveLeft():
+	$AnimatedSprite.animation = "walk"
 	$AnimatedSprite.flip_h = true
 	velocity.x = -SPEED
 func moveRight():
+	$AnimatedSprite.animation = "walk"
 	$AnimatedSprite.flip_h = false
 	velocity.x = +SPEED
 func idle():
+	$AnimatedSprite.animation = "idle"
 	velocity.x = 0
+func jump():
+	if is_on_floor():
+		on_floor = false
+		$AnimatedSprite.animation = "jump"
+		velocity.y -= JUMP
 	
 func  _process(delta):
+	if (is_on_floor()):
+		on_floor = true
+	else:
+		velocity.y +=G
+
 	if velocity.length() > 0:
-		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.play()
 	else:
-		$AnimatedSprite.animation = "idle"
 		$AnimatedSprite.stop()
 		
 	if Input.is_action_pressed("ui_left"):
 		moveLeft()
 	elif Input.is_action_pressed("ui_right"):
 		moveRight()
+	elif Input.is_action_pressed("ui_up"):
+		jump()
 	else:
 		idle()
 		
-	position += velocity * delta
+		
+	move_and_slide(velocity, Vector2( 0,-1 ))
+
 
 func _move_and_collide():
 	idle()
