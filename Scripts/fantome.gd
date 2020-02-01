fe8a6402a6691695b4006da048decb88f8af7146
pierre <pierre.garcia@u-psud.fr>
2020-02-01 16:20:51 +0100

    saw sounds

--- Scripts/fantome.gd
+++ Scripts/fantome.gd
@@ -5,7 +5,22 @@ extends KinematicBody2D
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
+var playerToChasePath = "player"
+export (float) var speed = 20
+onready var playerToChase = get_tree().get_root().get_node(playerToChasePath)
+
+func playerDirection():
+	return (self.position - playerToChase.position).normalized()
+
+func moveVector(delta):
+	return speed * Vector2(speed,0)
+
+func _process(delta):
+	print(get_tree().get_node_count())
+	move_and_slide(moveVector(delta))
+
 func _ready():
+	$AudioStreamPlayer2D.playing = true
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
