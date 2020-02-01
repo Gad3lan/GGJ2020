061e003a80d87d2a1e9c83471ba43bd4c6ec74d1
pierre <pierre.garcia@u-psud.fr>
2020-02-01 21:45:00 +0100

    hey

---
+++ Scripts/CanvasLayer.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+var screen_coord = get_viewport_transform() * (get_global_transform() * local_pos)
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
